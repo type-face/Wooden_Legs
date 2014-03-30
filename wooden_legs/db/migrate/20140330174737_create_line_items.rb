ab53dcd100cd93e83f4ba8d4d9d1ade88209a900
Rob Ring <rjm.ring@gmail.com>
2014-03-30 12:48:26 -0500

    Re-created Line Item model

--- wooden_legs/db/migrate/20140330174737_create_line_items.rb
+++ wooden_legs/db/migrate/20140330174737_create_line_items.rb
@@ -2,9 +2,9 @@ class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
       t.integer :quantity
-      t.decimal :price
-      t.reference :product_id
-      t.reference :order_id
+      t.decimal :price, precision: 10, scale: 2
+      t.reference :product
+      t.reference :order
 
       t.timestamps
     end
