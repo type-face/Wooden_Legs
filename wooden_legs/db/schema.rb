dd23f2a4a77d6a5ea73cc9bbe2161a3452253b05
Rob Ring <rjm.ring@gmail.com>
2014-03-30 13:27:46 -0500

    Ran db:migrate for 1st time

--- wooden_legs/db/schema.rb
+++ wooden_legs/db/schema.rb
@@ -11,6 +11,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140330180819) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "controllers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "customers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "postal_code"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "province_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "designers", force: true do |t|
+    t.string   "company"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "postal_code"
+    t.string   "email"
+    t.integer  "province_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "quantity"
+    t.decimal  "price",      precision: 10, scale: 2
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "order_statuses", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.decimal  "pst_rate",        precision: 5, scale: 2
+    t.decimal  "gst_rate",        precision: 5, scale: 2
+    t.decimal  "hst_rate",        precision: 5, scale: 2
+    t.integer  "order_status_id"
+    t.integer  "customer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price",          precision: 10, scale: 2
+    t.integer  "stock_quantity"
+    t.string   "image_src"
+    t.integer  "category_id"
+    t.integer  "designer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "provinces", force: true do |t|
+    t.string   "name"
+    t.decimal  "pst"
+    t.decimal  "get"
+    t.decimal  "hst"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
