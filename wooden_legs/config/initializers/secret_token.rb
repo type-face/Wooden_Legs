032854ae348f9acac0f76a6c3ef4e3fb62a47644
Rob Ring <rjm.ring@gmail.com>
2014-03-30 11:05:45 -0500

    First commit

---
+++ wooden_legs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WoodenLegs::Application.config.secret_key_base = '825ae231eaa1197dafd44af68afebaada71928e654bd3ae8f56a5671fbb970fc6f82c1e640765559ffa827e3e4a4105a9ccdc03b798dd59427256e9d46012da0'
---
+++ wooden_legs/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WoodenLegs::Application.config.session_store :cookie_store, key: '_wooden_legs_session'
