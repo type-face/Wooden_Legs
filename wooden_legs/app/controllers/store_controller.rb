f4958708ec3b49c7c0221481085e1f9902d8c30f
Rob Ring <rjm.ring@gmail.com>
2014-04-17 00:01:10 -0500

    Added pagination. Last commit before converting to Postgres

--- wooden_legs/app/controllers/store_controller.rb
+++ wooden_legs/app/controllers/store_controller.rb
@@ -1,8 +1,8 @@
 class StoreController < ApplicationController
-  before_filter :common_content
+  before_filter :common_content, only:[:index, :filter_by_category, :search_results]
 
   def index        
-    @products = Product.all 
+    @products = Product.all.paginate(page: params[:page], per_page: 6)    
   end
 
   def show
@@ -10,16 +10,15 @@ class StoreController < ApplicationController
   end
 
   def filter_by_category
-    @products = Product.where("category_id = ?", params[:id])
+    @products = Product.where("category_id = ?", params[:id]).paginate(page: params[:page], per_page: 6)
   end
 
   def search_results
-    @products = Product.search(params[:search_terms], params[:search_category])
+    @products = Product.search(params[:search_terms], params[:search_category]).paginate(page: params[:page], per_page: 6)
   end
 
   def common_content
     @categories = Category.all.order(:name)
   end
-
 
 end
